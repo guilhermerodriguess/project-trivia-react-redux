628656d1d020a3ec8cbc87073ea3bc17c181f574
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-04-05 19:03:13 -0300

    continuando requisito 05

--- src/component/Questions.jsx
+++ src/component/Questions.jsx
@@ -1,17 +1,69 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import store from '../redux/store';
+import { updateToken } from '../redux/actions';
 
 class Questions extends React.Component {
-  // requestAPI = async () => {
-  //   const token = '33c9a40c7a7cdd0eb860109014b8c87d20cb9f2f502abc674368506e308f7326';
-  //   const endPoint = `https://opentdb.com/api.php?amount=5&token=${token}`;
-  //   const response = await fetch(endPoint);
-  //   const responseJSOn = await response.json();
-  //   console.log(responseJSOn.results);
-  // }
+  constructor() {
+    super();
+    this.state = {
+      validToken: false,
+    };
+  }
+
+  componentDidMount() {
+    this.validateAPI();
+    store.subscribe(this.updateToken);
+  }
+
+  updateToken = () => {
+    this.setState((prevState) => ({
+      validToken: !prevState.validToken,
+    }));
+  }
+
+  requestAPI = async () => {
+    const { token } = this.props;
+    const endPoint = `https://opentdb.com/api.php?amount=5&token=${token}`;
+    const response = await fetch(endPoint);
+    const responseJSOn = await response.json();
+    console.log(responseJSOn);
+    return responseJSOn;
+  }
+
+  validateAPI = async () => {
+    const response = this.requestAPI();
+    const expirationCode = 3;
+    if (response.response_code === expirationCode) {
+      const { getToken } = this.props;
+      getToken();
+    }
+  }
 
   render() {
-    return (<p>Questions</p>);
+    const responseApi = this.requestAPI();
+    // const { category } = responseApi.results;
+    console.log(responseApi, 'resposta aqui');
+    return (
+      <div>
+        <h2 data-testid="question-category"> </h2>
+      </div>
+    );
   }
 }
 
-export default Questions;
+Questions.propTypes = {
+  getToken: PropTypes.func.isRequired,
+  token: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  token: state.token,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getToken: () => dispatch(updateToken()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Questions);
