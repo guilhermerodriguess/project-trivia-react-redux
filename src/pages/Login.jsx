fe8cc13ff6cf77b43e4e650dd91382be394ac2cd
lucasbaq <lucasbaquintela@gmail.com>
2022-04-05 15:57:23 -0300

    termina requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../component/LoginForm';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="login-page">
+        <LoginForm />
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/redux/actions/index.js
@@ -0,0 +1,6 @@
+export const UPDATE_LOGIN = 'UPDATE_LOGIN';
+
+export const updateLogin = (login) => ({
+  type: UPDATE_LOGIN,
+  login,
+});
