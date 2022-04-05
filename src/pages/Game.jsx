2f618ec44b34fb31c90a3c3201f8a890e53a1618
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-04-05 17:48:38 -0300

    iniciando requisito 05

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,36 +1,26 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import md5 from 'crypto-js/md5';
+import Header from '../component/Header';
+import Questions from '../component/Questions';
 
 class Game extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
   render() {
-    const { name, email } = this.props;
-    const convertEmail = md5(email).toString();
-    console.log(email, 'email');
-    console.log(convertEmail, 'convert');
     return (
-      <header>
-        <img alt="foto" src={ `https://www.gravatar.com/avatar/${convertEmail}` } data-testid="header-profile-picture" />
-        <h2 data-testid="header-player-name">
-          Nome:
-          {' '}
-          { name }
-        </h2>
-        <h2 data-testid="header-score">pontos: 0</h2>
-      </header>
+      <>
+        <Header />
+        <Questions />
+      </>
     );
   }
 }
 
 Game.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
+
 };
 
-const mapStateToProps = (state) => ({
-  name: state.loginReducer.player.name,
-  email: state.loginReducer.player.gravatarEmail,
-});
-
-export default connect(mapStateToProps)(Game);
+export default connect()(Game);
